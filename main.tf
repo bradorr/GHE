20f4337bf20d45e69665f933c53d224b133629c1
Bradley Orr <bradleyorr@BradleyOrrsMBP5.edi.cloudreach.com>
2018-10-25 10:52:27 +0100

    started terraform file

---
+++ main.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+    region = "${var.aws_region}"
+}
+
+resource "aws_vpc" "ghe-vpc" {
+    cidr_block = "${var.vpc_cidr}"
+}
+
+resource "aws_internet_gateway" "gw" {
+    vpc_id = "${aws_vpc.ghe-vpc.id}"
+}
+
+resource "aws_subnet" "main" {
+    vpc_id = "${aws_vpc.ghe-vpc.id}"
+    cidr_block = "${var.subnet_cidr}"
+    map_public_ip_on_launch = true
+}
+
+resource "aws_ebs_volume" "ghe" {
+    availability_zone = "${var.ebs_az}"
+    type = "${var.ebs.type}"
+}
---
+++ vars.tf
@@ -0,0 +1,15 @@
+aws_region = {
+    default = "eu-west-1"
+}
+
+vpc_cidr = {
+    default = "10.0.0.0/16"
+}
+
+subnet_cidr = { 
+    default = "10.0.1.0/24"
+}
+
+ebs_az = {
+    deafult = "eu-west-1a"
+}
